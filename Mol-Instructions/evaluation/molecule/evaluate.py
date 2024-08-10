2392794decb38999841ec7698d2fb878d8466411
WeeeicheN <dddwc@mail.ustc.edu.cn>
2024-08-10 17:45:33 +0800

    Update evaluate.py for Mol-Instructions

--- Mol-Instructions/evaluation/molecule/evaluate.py
+++ Mol-Instructions/evaluation/molecule/evaluate.py
@@ -15,11 +15,25 @@ def sf_encode(selfies):
 def sf_trans(formula):
     try:
         smiles = sf.decoder(formula)
-        if smiles == None or smiles == '': # 生成的可能直接就是smile表达式
+        if smiles == None or smiles == '': # 模型原本生成的可能直接就是smile表达式
             smiles = sf.decoder(sf.encoder(formula))
         return smiles
     except Exception:
-        return None
+        try:
+            temp_formula = formula.rsplit(']', 1)[0] + ']' # 应对极端情况：The potential reaction [C][C][C][O][C][Ring1][Branch1].[C][C][=Branch1][C][=O][O].[O] may occur.
+            smiles = sf.decoder(temp_formula)
+            return smiles
+        except Exception:
+            try:
+                temp_formula = formula.split(":", 1)[-1] # XXX is: YYY
+                temp_formula = temp_formula.strip()
+                temp_formula = temp_formula.rsplit(" ", 1)[-1] # XXX is YYY
+                temp_formula = temp_formula.strip()
+                temp_formula = temp_formula.strip(".")
+                smiles = sf.decoder(sf.encoder(temp_formula))
+                return smiles
+            except Exception:
+                return None
 
 def convert_to_canonical_smiles(smiles):
     if smiles is None:
@@ -60,12 +74,13 @@ def metrics(input_path, output_path, task):
     elif task == 'mol_gen':
         data.dropna(axis=0, how='any', inplace=True)
         
-        # data['output'] = data['output'].apply(lambda x: x.rsplit(']', 1)[0] + ']' if isinstance(x, str) else x)
-        data['output'] = data['output'].apply(lambda x: x.rsplit('<|end_of_text|>', 1)[0] if isinstance(x, str) else x)
+        # data['output'] = data['output'].apply(lambda x: x.rsplit(']', 1)[0] + ']' if isinstance(x, str) else x) # 应对极端情况：The potential reaction [C][C][C][O][C][Ring1][Branch1].[C][C][=Branch1][C][=O][O].[O] may occur. 但不应该放在这修改
+        # data['output'] = data['output'].apply(lambda x: x.rsplit('<', 1)[0] if isinstance(x, str) else x) # 删除终止符
+        data['output'] = data['output'].apply(lambda x: x.replace('<|eot_id|>', '').replace('<|end_of_text|>', '') if isinstance(x, str) else x) # 删除终止符
 
         # data['output_smiles'] = data['output'].map(sf_encode)
-        # input: sf_encode("This molecular's SMILES name is [C][C][=N][C][Branch1][C][C][=C][Branch1][#Branch1][C][=Branch1][C][=O][C][Br][NH1][Ring1][#Branch2]")
-        # output: 'CC1=NC(C)=C(C(=O)CBr)[NH1]1'
+        ## input: sf_encode("This molecular's SMILES name is [C][C][=N][C][Branch1][C][C][=C][Branch1][#Branch1][C][=Branch1][C][=O][C][Br][NH1][Ring1][#Branch2]")
+        ## output: 'CC1=NC(C)=C(C(=O)CBr)[NH1]1'
         data['output_smiles'] = data['output'].map(sf_trans)
         
         data.dropna(axis=0, how='any', inplace=True)
@@ -90,9 +105,10 @@ def metrics(input_path, output_path, task):
         data['SMILES_org'] = data['description'].map(sf_encode)
         data['SMILES'] = data['SMILES_org'].map(convert_to_canonical_smiles)
 
-        # data['output'] = data['output'].apply(lambda x: x.rsplit('.', 1)[0] + '.' if isinstance(x, str) else x)
+        data['output'] = data['output'].apply(lambda x: x.rsplit('.', 1)[0] + '.' if isinstance(x, str) else x) # 丢弃最后一个不完整的句子
         # # data['output'] = data['output'].str.rsplit('.', 1).str[0] + '.'
-        data['output'] = data['output'].apply(lambda x: x.rsplit('<|end_of_text|>', 1)[0] if isinstance(x, str) else x)
+        # data['output'] = data['output'].apply(lambda x: x.rsplit('<', 1)[0] if isinstance(x, str) else x) # 删除终止符
+        data['output'] = data['output'].apply(lambda x: x.replace('<|eot_id|>', '').replace('<|end_of_text|>', '') if isinstance(x, str) else x) # 删除终止符
 
         # data['ground truth'] = data['ground_truth']
         data.dropna(axis=0, how='any', inplace=True)
